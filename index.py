2f796b17d7fa751b1cbf3d904fe8ece4d8657fed
GhaloGhertje <koenvdh7@gmail.com>
2022-01-27 19:10:13 +0100

    update

--- index.py
+++ index.py
@@ -3,8 +3,10 @@ from utils.start import Start
 from utils.insert import Insert
 from utils.stop import Stop
 from utils.reset import Reset
+from utils.train import Train
 
 Start.start
 Insert.insert
 Stop.stop
 Reset.reset
+Train.train
---
+++ utils/train.py
@@ -0,0 +1,9 @@
+import pygame
+
+class Train():
+    def __init__():
+        insert = print('trijntje oosterhuis')
+
+    #def update():
+
+    #def draw():
