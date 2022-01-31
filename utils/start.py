58a1eb8853bc046d82db6d1f72775ff971e80ebb
GhaloGhertje <koenvdh7@gmail.com>
2022-01-31 20:02:50 +0100

    fix

--- utils/start.py
+++ utils/start.py
@@ -3,13 +3,13 @@ import pygame, ctypes
 class Start():
     def screen():
         print('start')
+        ctypes.windll.user32.SetProcessDPIAware() # Sommige computers werken niet zonder deze functie
 
         pygame.init() # Start pygame op
         pygame.display.set_caption('Speciale relativiteitstheorie') # Geeft een naam aan de applicatie
 
         # Display functies
         display_info = pygame.display.Info()
-        ctypes.windll.user32.SetProcessDPIAware() # Sommige computers werken niet zonder deze functie
         screen = pygame.display.set_mode((display_info.current_w, display_info.current_h), pygame.NOFRAME) # displayInfo.current_w staat voor de width van het scherm die al eerder opgevraagd is. pygame.NOFRAME staat voor windowed borderless
         return screen
 
