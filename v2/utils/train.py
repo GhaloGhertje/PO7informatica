8c2b3b719ef9e745fa96f87f6fbb235fd40f6e0e
Nico <nico.nap@outlook.com>
2022-02-08 09:28:27 +0100

    Bug fix: path naam was niet correct

--- v2/utils/train.py
+++ v2/utils/train.py
@@ -1,5 +1,6 @@
 # IMPORT LIBRARIES
-import pygame, os
+import pygame
+import os
 
 
 # CLASS
@@ -11,32 +12,41 @@ class Train():
         self.x_mid_position /= 2
         self.y_mid_position /= 2
 
-        self.old_velocity = -1 # old_velocity moet gedefinieerd moet worden, maar de waarde mag niet gelijk zijn of groter dan zijn dan 0 aan het begin.
+        # old_velocity moet gedefinieerd moet worden, maar de waarde mag niet gelijk zijn of groter dan zijn dan 0 aan het begin.
+        self.old_velocity = -1
 
-        self.original_image = pygame.image.load(os.path.join('utils', 'images', image_name)) # os.path.join() zorgt ervoor dat het programma ook op ander besturingssystemen afgespeeld kan worden, omdat paths anders werken op verschillende besturingssystemen
+        # os.path.join() zorgt ervoor dat het programma ook op ander besturingssystemen afgespeeld kan worden, omdat paths anders werken op verschillende besturingssystemen
+        self.original_image = pygame.image.load(
+            os.path.join('v2', 'utils', 'images', image_name))
         self.original_image_rectangle = self.original_image.get_rect()
         self.length, self.height = self.original_image_rectangle.size
 
         self.length /= 4
         self.height /= 4
 
-        self.image = pygame.transform.scale(self.original_image, self.original_image_rectangle.size)
+        self.image = pygame.transform.scale(
+            self.original_image, self.original_image_rectangle.size)
         self.font = font
 
-
     def update(self, velocity, gamma_factor):
-        if velocity != self.old_velocity: # Beperkt het aantal keer dat de trein geupdate moet worden als er niets veranderd is
+        if velocity != self.old_velocity:  # Beperkt het aantal keer dat de trein geupdate moet worden als er niets veranderd is
             self.old_velocity = velocity
 
             self.reference_length = self.length/gamma_factor
             self.percentage = (1/gamma_factor)*100
-            self.rectangle = pygame.Rect(self.x_mid_position-(self.reference_length/2), self.y_mid_position-(self.height/2), self.reference_length, self.height)
+            self.rectangle = pygame.Rect(self.x_mid_position-(self.reference_length/2),
+                                         self.y_mid_position-(self.height/2), self.reference_length, self.height)
 
-            self.image = pygame.transform.scale(self.original_image, self.rectangle.size)
-
+            self.image = pygame.transform.scale(
+                self.original_image, self.rectangle.size)
 
     def draw(self):
-        self.screen.blit(self.image, (self.rectangle.x, self.rectangle.y)) # Plaatje als trein
-        self.percentage_txt = self.font.render(str(int(round(self.percentage, 0))) + "%", False, (255,255, 255)) # int() doet niet hetzelfde als round(), int() haalt de decimalen weg zonder af te ronden
-        self.percentage_rect = self.percentage_txt.get_rect(center=(self.x_mid_position, self.y_mid_position-80))
-        self.screen.blit(self.percentage_txt, self.percentage_rect) # Percentage lengte van de trein
+        # Plaatje als trein
+        self.screen.blit(self.image, (self.rectangle.x, self.rectangle.y))
+        # int() doet niet hetzelfde als round(), int() haalt de decimalen weg zonder af te ronden
+        self.percentage_txt = self.font.render(
+            str(int(round(self.percentage, 0))) + "%", False, (255, 255, 255))
+        self.percentage_rect = self.percentage_txt.get_rect(
+            center=(self.x_mid_position, self.y_mid_position-80))
+        # Percentage lengte van de trein
+        self.screen.blit(self.percentage_txt, self.percentage_rect)
