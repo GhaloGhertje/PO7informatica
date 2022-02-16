9e47e29c1196c890d6fc9dfde701144e9e98f3d8
Koen <koenvdh7@gmail.com>
2022-02-16 11:13:29 +0100

    Update clock.py

--- v2/utils/clock.py
+++ v2/utils/clock.py
@@ -21,6 +21,8 @@ class Clock():
         #self.surface = pygame.surface.Surface(self.rectangle)
 
         self.start = False
+        self.start_draw = False
+        self.power = 0
 
 
     def update(self):
@@ -50,8 +52,13 @@ class Clock():
         self.text_render = self.font.render(self.text, False, RED)
         self.text_width, self.text_height = self.font.size(self.text)
         
-        self.rect_border = pygame.Rect(self.coordinates[0] -50, self.coordinates[1], self.text_width +100, self.text_height +50)
-        self.rect_background = pygame.Rect(self.coordinates[0] -25, self.coordinates[1] +25, self.text_width +50, self.text_height)
+        if (round(self.time, 0) == 10**self.power or not self.start_draw):
+            self.start_draw = True
+
+            self.power += 1
+            self.rect_border = pygame.Rect(self.coordinates[0] -50, self.coordinates[1], self.text_width +100, self.text_height +50)
+            self.rect_background = pygame.Rect(self.coordinates[0] -25, self.coordinates[1] +25, self.text_width +50, self.text_height)
+
         pygame.draw.rect(self.screen, WHITE, self.rect_border)
         pygame.draw.rect(self.screen, BLACK, self.rect_background)
 
